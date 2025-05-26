4d364fea2fb626a195bee48ce5683f28522befd4
davidvillon04 <davidv04@vt.edu>
2025-05-26 14:20:35 -0400

    Use firebase admin

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,9 +1,20 @@
-const { initializeApp } = require("firebase/app");
-const { getFirestore } = require("firebase/firestore");
+// const { initializeApp } = require("firebase/app");
+// const { getFirestore } = require("firebase/firestore");
+
+// const serviceAccount = require("./permissions.json");
+
+// const app = initializeApp(serviceAccount);
+// const db = getFirestore(app);
+
+// module.exports = db;
+
+const admin = require("firebase-admin");
 
 const serviceAccount = require("./permissions.json");
 
-const app = initializeApp(serviceAccount);
-const db = getFirestore(app);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
+const db = admin.firestore();
 module.exports = db;
